3127ed0cf821bb07292c9585eccd6f1bb5e30b25
nicholasturner1 <nturner.stanford@gmail.com>
2017-08-20 15:18:43 -0400

    Exposing only useful fns from utils module

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,4 +1,5 @@
-from async_sampler import *
-from learning_monitor import *
-from sample_spec import *
-from utils import *
+from async_sampler import AsyncSampler
+from learning_monitor import LearningMonitor
+from sample_spec import SampleSpec
+from utils import log_tagged_modules, save_chkpt, load_chkpt, iter_from_chkpt_fname
+from utils import masks_not_empty, make_variable, read_h5, write_h5
