6d3704142c53d990e4e15b3bd9b56ca765531ba4
nicholasturner1 <nturner.stanford@gmail.com>
2017-10-10 16:17:31 -0400

    Some cleaning, starting with run_exp.py

--- src/run_exp.py
+++ src/run_exp.py
@@ -1,109 +1,30 @@
 #!/usr/bin/env python
+__doc__ = """
 
-import os, re, imp, sys
-import shutil
+Training Script
+
+Nicholas Turner, 2017
+"""
+
+import os, imp
 import collections
-import time
 
 import torch
 
-import train
 import utils
-
-
-EXPT_NAME     = sys.argv[1]
-GPU           = sys.argv[2]
-SAMPLER_FNAME = sys.argv[3]
-MODEL_FNAME   = sys.argv[4]
-BATCH_SIZE    = int(sys.argv[5]) if len(sys.argv) > 5 else 1
-CHKPT_NUM     = int(sys.argv[6]) if len(sys.argv) > 6 else 0
-
-
-os.environ["CUDA_VISIBLE_DEVICES"] = GPU
-
-
-params = dict()
-
-#Model params
-params["in_dim"]       = 1
-params["output_spec"]  = collections.OrderedDict(affinity_label=12)
-params["depth"]        = 4
-params["batch_norm"]   = True
-
-#Training procedure params
-params["max_iter"]    = 1000000
-params["lr"]          = 0.00001
-params["test_intv"]   = 1000
-params["test_iter"]   = 100
-params["avgs_intv"]   = 50
-params["chkpt_intv"]  = 10000
-params["chkpt_num"]   = CHKPT_NUM
-params["batch_size"]  = BATCH_SIZE
-
-#IO/Record params
-params["expt_dir"]   = "experiments/{}".format(EXPT_NAME)
-params["model_dir"]  = os.path.join(params["expt_dir"], "models")
-params["log_dir"]    = os.path.join(params["expt_dir"], "logs")
-params["fwd_dir"]    = os.path.join(params["expt_dir"], "forward")
-
-#Sampling params
-params["data_dir"]     = os.path.expanduser("~/seungmount/research/Nick/datasets/SNEMI3D/")
-params["train_sets"]   = ["train"]
-params["val_sets"]     = ["val"]
-
-
-#modules used for record-keeping
-params["modules_used"] = [__file__, MODEL_FNAME, "layers.py",
-                          SAMPLER_FNAME, "loss.py"]
-
-#Use-specific Module imports
+import train
 import loss
-sampler_module = imp.load_source("Sampler",SAMPLER_FNAME)
-model_module = imp.load_source("Model",MODEL_FNAME)
-Model = model_module.Model
 
 
+def main(**args):
 
-def start_training(in_dim, output_spec, depth, batch_norm, chkpt_num,
-                   lr, train_sets, val_sets, data_dir, **params):
+    #args should be the info you need to specify the params
+    # for a given experiment, but only params should be used below
+    params = fill_params(**args)
 
-    print("Loading model...")
-    start = time.time()
+    set_gpus(params["gpus"])
 
-    net = torch.nn.DataParallel(Model(in_dim, output_spec, depth, bn=batch_norm)).cuda()
-    monitor = utils.LearningMonitor()
-
-    if chkpt_num != 0:
-        utils.load_chkpt(net, monitor, chkpt_num,
-                         params["model_dir"],
-                         params["log_dir"])
-
-    loss_fn = loss.BinomialCrossEntropyWithLogits()
-
-    optimizer = torch.optim.Adam( net.parameters(), lr=lr )
-
-    print("Finished in {} seconds".format(time.time() - start))
-
-    train_sampler = utils.AsyncSampler(sampler_module.Sampler(data_dir,
-                                                      mode="train"))
-
-    val_sampler   = utils.AsyncSampler(sampler_module.Sampler(data_dir,
-                                                      mode="val"))
-
-    train.train(net, loss_fn, optimizer, train_sampler, val_sampler,
-                last_iter=chkpt_num, monitor=monitor, **params)
-
-
-def make_reqd_dirs(model_dir, log_dir, fwd_dir, **params):
-
-    for d in [model_dir, log_dir, fwd_dir]:
-      if not os.path.isdir(d):
-        os.makedirs(d)
-
-
-def main(**params):
-
-    make_reqd_dirs(**params)
+    make_required_dirs(**params)
 
     utils.log_tagged_modules(params["modules_used"],
                              params["log_dir"], "train",
@@ -111,5 +32,119 @@ def main(**params):
 
     start_training(**params)
 
+
+def fill_params(expt_name, chkpt_num, batch_sz, gpus,
+                sampler_fname, model_fname, **args):
+
+    params = dict()
+
+    #Model params
+    params["in_dim"]       = 1
+    params["output_spec"]  = collections.OrderedDict(psd_label=1)
+    params["depth"]        = 4
+    params["batch_norm"]   = True
+
+    #Training procedure params
+    params["max_iter"]    = 1000000
+    params["lr"]          = 0.00001
+    params["test_intv"]   = 1000
+    params["test_iter"]   = 100
+    params["avgs_intv"]   = 50
+    params["chkpt_intv"]  = 10000
+    params["chkpt_num"]   = chkpt_num
+    params["batch_size"]  = batch_sz
+
+    #Sampling params
+    params["data_dir"]     = os.path.expanduser("~/seungmount/research/Nick/datasets/SNEMI3D/")
+    assert os.path.isdir(params["data_dir"]),"nonexistent data directory"
+    params["train_sets"]   = ["train"]
+    params["val_sets"]     = ["val"]
+
+    #GPUS
+    params["gpus"] = gpus
+
+    #IO/Record params
+    params["expt_name"]  = expt_name
+    params["expt_dir"]   = "experiments/{}".format(expt_name)
+    params["model_dir"]  = os.path.join(params["expt_dir"], "models")
+    params["log_dir"]    = os.path.join(params["expt_dir"], "logs")
+    params["fwd_dir"]    = os.path.join(params["expt_dir"], "forward")
+
+    #Use-specific Module imports
+    params["sampler_class"] = imp.load_source("Sampler",sampler_fname).Sampler
+    params["model_class"]    = imp.load_source("Model",model_fname).Model
+    #"Schema" for turning the parameters above into arguments
+    # for the model class
+    params["model_args"]     = [ params["in_dim"], params["output_spec"],
+                                 params["depth"] ]
+    params["model_kwargs"]   = { "bn" : params["batch_norm"] }
+
+    #modules used for record-keeping
+    params["modules_used"] = [__file__, model_fname, "layers.py",
+                              sampler_fname, "loss.py"]
+
+    return params
+
+
+def set_gpus(gpu_list):
+    os.environ["CUDA_VISIBLE_DEVICES"] = " ".join(gpu_list)
+
+
+def make_required_dirs(model_dir, log_dir, fwd_dir, **params):
+
+    for d in [model_dir, log_dir, fwd_dir]:
+      if not os.path.isdir(d):
+        os.makedirs(d)
+
+
+def start_training(model_args, model_kwargs, chkpt_num,
+                   lr, train_sets, val_sets, data_dir, **params):
+
+    #PyTorch Model
+    Model = params["model_class"]
+    net = torch.nn.DataParallel(Model(*model_args, **model_kwargs)).cuda()
+    monitor = utils.LearningMonitor()
+
+    #Loading model checkpoint (if applicable)
+    if chkpt_num != 0:
+        utils.load_chkpt(net, monitor, chkpt_num,
+                         params["model_dir"],
+                         params["log_dir"])
+
+    #DataProvider Sampler
+    Sampler = params["sampler_class"]
+    train_sampler = utils.AsyncSampler(Sampler(data_dir, dsets=train_sets,
+                                               mode="train"))
+
+    val_sampler   = utils.AsyncSampler(Sampler(data_dir, dsets=val_sets,
+                                               mode="val"))
+
+    loss_fn = loss.BinomialCrossEntropyWithLogits()
+    optimizer = torch.optim.Adam( net.parameters(), lr=lr )
+
+    train.train(net, loss_fn, optimizer, train_sampler, val_sampler,
+                last_iter=chkpt_num, monitor=monitor, **params)
+
+
 if __name__ == "__main__":
-    main(**params)
+
+    import argparse
+
+    parser = argparse.ArgumentParser(description= __doc__)
+
+    parser.add_argument("expt_name",
+                        help="Experiment Name")
+    parser.add_argument("sampler_fname",
+                        help="DataProvider Sampler Filename")
+    parser.add_argument("model_fname",
+                        help="Model Template Filename")
+    parser.add_argument("--batch_sz", default=1,
+                        help="Batch size for each sample")
+    parser.add_argument("--chkpt_num", default=0,
+                        help="Checkpoint Number")
+    parser.add_argument("--gpus", default=["0"], nargs="+")
+
+    args = parser.parse_args()
+
+
+    main(**vars(args))
--- src/samplers/psd_sampler.py
+++ src/samplers/psd_sampler.py
@@ -8,17 +8,25 @@ Nicholas Turner <nturner@cs.princeton.edu>, 2017
 import os
 
 import dataprovider as dp
-import u
+import h5py
+
+def read_file(fname):
+
+    f = h5py.File(fname)
+    d = f["/main"].value
+    f.close()
+
+    return d
 
 
 class Sampler(object):
 
     def __init__(self, datadir, dsets=[], mode="train", patchsz=(18,160,160)):
 
-      assert mode in ["train","test"]
+      assert mode in ["train","val","test"]
 
       datadir = os.path.expanduser(datadir)
-  
+
       volnames = ["input","psd_label","psd_mask"]
       spec = { name : patchsz for name in volnames }
 
@@ -35,7 +43,7 @@ class Sampler(object):
 
       for dset in dsets:
         vdp.add_dataset( self.build_dataset(datadir, spec, dset) )
-      
+
       vdp.set_sampling_weights()
 
       vdp.set_augmentor(self._aug(mode))
@@ -47,9 +55,9 @@ class Sampler(object):
     def build_dataset(self, datadir, spec, dset_name):
 
       print(dset_name)
-      img = u.read_file(os.path.join(datadir, dset_name + "_img.h5"))      
-      psd = u.read_file(os.path.join(datadir, dset_name + "_syn.h5")).astype("float32")
-      seg = u.read_file(os.path.join(datadir, dset_name + "_seg.h5"))
+      img = read_file(os.path.join(datadir, dset_name + "_img.h5"))
+      psd = read_file(os.path.join(datadir, dset_name + "_syn.h5")).astype("float32")
+      seg = read_file(os.path.join(datadir, dset_name + "_seg.h5"))
 
       img = dp.transform.divideby(img, val=255.0, dtype="float32")
       psd[psd != 0] = 1 #Binarizing psds
@@ -59,7 +67,7 @@ class Sampler(object):
       vd.add_raw_data(key="input",      data=img)
       vd.add_raw_data(key="psd_label",  data=psd)
       vd.add_raw_data(key="psd_mask",   data=msk)
-      
+
       vd.set_spec(spec)
       return vd
 
@@ -81,4 +89,3 @@ class Sampler(object):
     def _post(self):
       post = dp.Transformer()
       return post
-
